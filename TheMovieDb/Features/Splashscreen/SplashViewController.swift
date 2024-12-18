d89c0f2171a21f01d8a59ac9adfbb5bf8172cf48
M. Rizki Maulana <devemrizkiem@gmail.com>
2024-12-18 10:39:53 +0700

    Setup splashscreen

---
+++ TheMovieDb/Features/Splashscreen/SplashViewController.swift
@@ -0,0 +1,53 @@
+//
+//  SplashViewController.swift
+//  TheMovieDb
+//
+//  Created by M. Rizki Maulana on 18/12/24.
+//
+
+import UIKit
+
+class SplashViewController: BaseViewController {
+  let logoImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.image = Images.Common.imgSplash
+    imageView.contentMode = .scaleAspectFit
+    imageView.translatesAutoresizingMaskIntoConstraints = false
+    return imageView
+  }()
+  
+  override func configureViews() {
+    view.backgroundColor = Color.primaryColor
+    
+    setupLogo()
+    startBounceAnimation()
+  }
+  
+  private func setupLogo() {
+    view.addSubview(logoImageView)
+
+    NSLayoutConstraint.activate([
+      logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+      logoImageView.widthAnchor.constraint(equalToConstant: 200),
+      logoImageView.heightAnchor.constraint(equalToConstant: 200)
+    ])
+  }
+  
+  private func startBounceAnimation() {
+    logoImageView.transform = CGAffineTransform(scaleX: 0.8, y: 0.8)
+    
+    UIView.animate(
+      withDuration: 1.0,
+      delay: 0,
+      usingSpringWithDamping: 0.5,
+      initialSpringVelocity: 1.0,
+      options: .curveEaseInOut,
+      animations: {
+        self.logoImageView.transform = .identity
+      },
+      completion: { _ in
+      // Transition to main content after animation
+    })
+  }
+}
